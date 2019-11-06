884db7c44adcd63e5a8cf700f2ac3b64aaa241e5
Paulo Fernando <paulo.fdo@hotmail.com>
2019-11-05 21:44:35 -0300

    Mantido único projeto como solução para cálculo do quadrado.

---
+++ ProblemaClassicoCalculoDaAreaSolucao/Retangulo02.cs
@@ -0,0 +1,13 @@
+﻿namespace ProblemaClassicoCalculoDaAreaSolucao
+{
+    internal class Retangulo02
+    {
+        public int Altura { get; set; }
+        public int Largura { get; set; }
+    }
+
+    internal class Quadrado02
+    {
+        public int Lado { get; set; }
+    }
+}
